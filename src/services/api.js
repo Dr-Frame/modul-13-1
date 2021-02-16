1bb18712e0c06dd3e07d36738607f066b65a91b8
Vlad Dr.Frame <remenjuk2010@gmail.com>
2021-02-16 08:13:55 +0200

    first comit

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,12 @@
 const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTlmMWJkNmIxODkyMTAwMTc1MGE5MWYiLCJpYXQiOjE1ODc0ODU2NTR9.r6HnAMqE1znCkYbvq8AOCd-NdMiH8O2-v4Nw5lwivio";
-
-const baseUrl = "https://goit-phonebook-api.herokuapp.com";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDJhMDc5MjY0ZjViYzAwMTdhMWUyZWIiLCJpYXQiOjE2MTMzNjcxODZ9.VF6NDDGgHimRNKGuDANwkih4GXWbSr5JWjkkaM8hZoo";
 
 const headers = {
   Authorization: `Bearer ${token}`,
 };
 
 const fetchContacts = () => {
-  return fetch(`${baseUrl}/contacts`, {
+  return fetch("https://goit-phonebook-api.herokuapp.com/contacts", {
     headers,
   }).then((res) => res.json());
 };
@@ -22,30 +20,38 @@ const createContact = (name, number) => {
     },
     body: JSON.stringify({ name, number }),
   };
-
-  return fetch(`${baseUrl}/contacts`, options).then((res) => res.json());
+  return fetch(
+    "https://goit-phonebook-api.herokuapp.com/contacts",
+    options
+  ).then((res) => res.json());
 };
 
 const deleteContact = (id) => {
   const options = {
     method: "DELETE",
-    headers,
+    headers: {
+      ...headers,
+      "Content-Type": "application/json",
+    },
   };
 
-  return fetch(`${baseUrl}/contacts/${id}`, options).then((res) => res.json());
+  return fetch(
+    `https://goit-phonebook-api.herokuapp.com/contacts/${id}`,
+    options
+  ).then((res) => res.json());
 };
 
 const updateContact = (id, fields) => {
   const options = {
     method: "PATCH",
-    headers: {
-      ...headers,
-      "Content-Type": "application/json",
-    },
+    headers: { ...headers, "Content-Type": "application/json" },
     body: JSON.stringify(fields),
   };
 
-  return fetch(`${baseUrl}/contacts/${id}`, options).then((res) => res.json());
+  return fetch(
+    `https://goit-phonebook-api.herokuapp.com/contacts/${id}`,
+    options
+  ).then((res) => res.json());
 };
 
 export default { fetchContacts, createContact, deleteContact, updateContact };
